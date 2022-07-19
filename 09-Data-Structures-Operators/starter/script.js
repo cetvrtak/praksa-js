65cb66519205074e4a0294c95ca9d2ea786d6dda
cetvrtak <59363714+cetvrtak@users.noreply.github.com>
2022-07-19 14:41:02 +0200

    Section 9: Maps

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -36,6 +36,92 @@ const restaurant = {
   },
 };
 
+// const question = new Map([
+//   ["question", "What is the best programming language in the world?"],
+//   [1, "C"],
+//   [2, "Java"],
+//   [3, "JavaScript"],
+//   ["correct", 3],
+//   [true, "Correct answer"],
+//   [false, "Try again"],
+// ]);
+// console.log(question);
+
+// // Convert object to map
+// console.log(Object.entries(restaurant.openingHours));
+// const hoursMap = new Map(Object.entries(restaurant.openingHours));
+// console.log(hoursMap);
+
+// // Quiz app
+// console.log(question.get("question"));
+// for (const [key, value] of question) {
+//   if (typeof key == "number") console.log(`Answer ${key}: ${value}`);
+// }
+// // const answer = prompt("Your answer");
+// const answer = 3;
+// console.log(answer);
+// console.log(question.get(question.get("correct") == answer));
+
+// // Convert map to array
+// console.log([...question]);
+// // console.log([...question.entries()]);
+// console.log([...question.keys()]);
+// console.log([...question.values()]);
+
+// Maps: Fundamentals
+// const rest = new Map();
+// rest.set("name", "Classico Italiano");
+// rest.set(1, "Firenze");
+// console.log(rest.set(2, "Lisbon"));
+// rest
+//   .set("categories", ["Italian", "Pizzeria", "Vegetarian", "Organic"])
+//   .set("open", 11)
+//   .set("close", 23)
+//   .set(true, "We are open")
+//   .set(false, "We are closed");
+
+// console.log(rest.get("name"));
+// console.log(rest.get(true));
+// console.log(rest.get(1));
+
+// const time = 21;
+// console.log(rest.get(time > rest.get("open") && time < rest.get("close")));
+
+// console.log(rest.has("categories"));
+// rest.delete(2);
+// // rest.clear();
+
+// const arr = [1, 2];
+// rest.set(arr, "Test");
+// rest.set(document.querySelector("h1"), "Heading");
+
+// console.log(rest);
+// console.log(rest.size);
+
+// console.log(rest.get(arr));
+
+// let ordersSet = new Set(["Pizza", "Pasta", "Risotto"]);
+// console.log(ordersSet);
+// console.log(ordersSet.size);
+// console.log(ordersSet.has("Pizza"));
+// console.log(ordersSet.has("Breadk"));
+// ordersSet.add("Garlic Bread");
+// ordersSet.add("Garlic Bread");
+// ordersSet.delete("Risotto");
+// // ordersSet.clear();
+// console.log(ordersSet);
+
+// for (const order of ordersSet) console.log(order);
+
+// // Example
+// const staff = ["Waiter", "Chef", "Waiter", "Manager", "Chef", "Waiter"];
+// const staffUnique = [...new Set(staff)];
+// console.log(staffUnique);
+// console.log(
+//   new Set(["Waiter", "Chef", "Waiter", "Manager", "Chef", "Waiter"]).size
+// );
+// console.log(new Set("StevoIliskovic").size);
+
 // const [starter, mainCourse] = restaurant.order(2, 0);
 // console.log(starter, mainCourse);
 
