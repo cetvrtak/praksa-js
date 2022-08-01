be7dbf745629f935fcc1ba62b0412dd37f965f16
cetvrtak <59363714+cetvrtak@users.noreply.github.com>
2022-08-01 11:20:48 +0200

    Section 13: Building a Tabbed Component

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -196,31 +196,54 @@ document.querySelector(".nav__links").addEventListener("click", function (e) {
 ///////////////////////////////////////////
 // DOM Traversing
 ///////////////////////////////////////////
-const h1 = document.querySelector("h1");
+// const h1 = document.querySelector("h1");
 
-// Children
-console.log(h1.querySelectorAll(".highlight"));
-console.log(h1.childNodes);
-console.log(h1.children);
-h1.firstElementChild.style.color = "red";
-h1.lastElementChild.style.color = "white";
+// // Children
+// console.log(h1.querySelectorAll(".highlight"));
+// console.log(h1.childNodes);
+// console.log(h1.children);
+// h1.firstElementChild.style.color = "red";
+// h1.lastElementChild.style.color = "white";
 
-// Parents
-console.log(h1.parentNode);
-console.log(h1.parentElement);
+// // Parents
+// console.log(h1.parentNode);
+// console.log(h1.parentElement);
 
-h1.closest(".header").style.background = "var(--gradient-secondary)";
+// h1.closest(".header").style.background = "var(--gradient-secondary)";
 
-h1.closest("h1").style.background = "var(--gradient-primary)";
+// h1.closest("h1").style.background = "var(--gradient-primary)";
 
-// Siblings
-console.log(h1.previousElementSibling);
-console.log(h1.nextElementSibling);
+// // Siblings
+// console.log(h1.previousElementSibling);
+// console.log(h1.nextElementSibling);
 
-console.log(h1.previousSibling);
-console.log(h1.nextSibling);
+// console.log(h1.previousSibling);
+// console.log(h1.nextSibling);
 
-console.log(h1.parentElement.children);
-[...h1.parentElement.children].forEach(function (el) {
-  if (el != h1) el.style.transform = "scale(0.5)";
+// console.log(h1.parentElement.children);
+// [...h1.parentElement.children].forEach(function (el) {
+//   if (el != h1) el.style.transform = "scale(0.5)";
+// });
+
+////////////////////////////////////////
+// Building a Tabbed Component
+////////////////////////////////////////
+const tabs = document.querySelectorAll(".operations__tab");
+const tabsContainer = document.querySelector(".operations__tab-container");
+const tabsContent = document.querySelectorAll(".operations__content");
+
+tabsContainer.addEventListener("click", function (e) {
+  const clicked = e.target.closest(".operations__tab");
+
+  if (!clicked) return;
+
+  tabs.forEach((t) => t.classList.remove("operations__tab--active"));
+  tabsContent.forEach((c) => c.classList.remove("operations__content--active"));
+
+  clicked.classList.add("operations__tab--active");
+
+  // Actuvate content
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add("operations__content--active");
 });
