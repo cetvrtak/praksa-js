b4dfb0041255e60d1520c6bfd8b22ddc5ee63dd1
cetvrtak <59363714+cetvrtak@users.noreply.github.com>
2022-08-03 13:09:15 +0200

    Section 15: Displaying a map marker

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -1,12 +1,57 @@
-'use strict';
+"use strict";
 
 // prettier-ignore
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
-const form = document.querySelector('.form');
-const containerWorkouts = document.querySelector('.workouts');
-const inputType = document.querySelector('.form__input--type');
-const inputDistance = document.querySelector('.form__input--distance');
-const inputDuration = document.querySelector('.form__input--duration');
-const inputCadence = document.querySelector('.form__input--cadence');
-const inputElevation = document.querySelector('.form__input--elevation');
+const form = document.querySelector(".form");
+const containerWorkouts = document.querySelector(".workouts");
+const inputType = document.querySelector(".form__input--type");
+const inputDistance = document.querySelector(".form__input--distance");
+const inputDuration = document.querySelector(".form__input--duration");
+const inputCadence = document.querySelector(".form__input--cadence");
+const inputElevation = document.querySelector(".form__input--elevation");
+
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(
+    function (p) {
+      //   const { latitude } = p.coords;
+      //   const { longitude } = p.coords;
+      const [latitude, longitude] = [41.0106888, 28.9658794];
+
+      console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+
+      const coords = [latitude, longitude];
+      const map = L.map("map").setView(coords, 14);
+
+      L.tileLayer("https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      L.marker(coords)
+        .addTo(map)
+        .bindPopup("A pretty CSS3 popup.<br> Easily customizable.")
+        .openPopup();
+
+      map.on("click", function (e) {
+        const { lat, lng } = e.latlng;
+        L.marker([lat, lng])
+          .addTo(map)
+          .bindPopup(
+            L.popup({
+              maxWidth: 250,
+              minWidth: 100,
+              autoClose: false,
+              closeOnClick: false,
+              className: "running-popup",
+            })
+          )
+          .setPopupContent("Walk in the park")
+          .openPopup();
+      });
+    },
+    function () {
+      alert("Could not get your position");
+    }
+  );
+}
